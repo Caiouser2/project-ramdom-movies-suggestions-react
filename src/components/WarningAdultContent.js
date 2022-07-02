b99c14d14c3c9f813335c4845838d6204cb23098
Caiouser2 <ocaio997@gmail.com>
2022-07-02 20:01:24 -0300

    resolve bug warning adult content

--- src/components/WarningAdultContent.js
+++ src/components/WarningAdultContent.js
@@ -1,27 +1,29 @@
+import {useState} from 'react'
 import './WarningAdultContent.css';
 
 export default function WarningAdultContent() {
-    let warningUser = localStorage.getItem("warningUser");
-    let warning = '';
+    let warning = localStorage.getItem("warning") //initial value undefined
 
-    const hideWarningUser = () => {
-        warningUser = localStorage.setItem("warningUser", "enabled");
-        warningUser = localStorage.getItem("warningUser");
+    const [activeWarning, setActiveWarning] = useState(warning);
+
+    function hideWarning() {
+        warning = localStorage.setItem("warning", 'hide'); //quando recebe o click altera o valor undenfided para hide
+        warning = localStorage.getItem("warning"); //o valor vai continuar sempre assim pois ficará salvo no local storage 
+        setActiveWarning(warning);
     }
 
-    if (warningUser === 'enabled') {
-        warningUser = localStorage.getItem("warningUser");
-        warning = 'hide';
-    }
+
 
     return(
-        <div className={'warning-adult-content ' + warning}>
+        <div className={'warning-adult-content ' + activeWarning}>
             <h4>
                 Antes de assistir qualquer filme ou série que foi lhe foi sugerido, 
                 verifique a classificação indicativa e certifique-se que você
                 faz parte do grupo permitido. Para mais informações ler (INFORMAÇÕES IMPORTANTES) no fim da pagína.
             </h4>
-            <h2 onClick={hideWarningUser}>X</h2>
+            <div className="container-cancel-pop-up-adult-content">
+                <h2 onClick={hideWarning}>X</h2>
+            </div>
         </div>
     );    
 }
