e68665ebb204b46d9360864eded07682311aede3
Caiouser2 <ocaio997@gmail.com>
2022-09-29 10:59:22 -0300

    refactor interface

--- src/UI/Overview.js
+++ src/UI/Overview.js
@@ -13,18 +13,18 @@ const Overview = props => {
 
     return(
         <div className="overview ">
-            <h2 tabIndex="0" title="Sinopse">Sinopse</h2>
+            <h2 tabIndex="0" title="Sinopse">Informações</h2>
             <h3 tabIndex="0" className={errorSelectEmpty} title={'Sinopse da obra'}>
+                <strong>Sinopse: </strong> 
                 {
                     props.warningError === 'error'
-                    ? 'Ops... você precisa escolher uma opção entre filme ou série.'
+                    ? ' Ops... você precisa escolher uma opção entre filme ou série.'
                     : props.warningError === 'fetch-failed'
-                    ? 'Ops... parece que nosso serviço está indisponível no momento, agurade alguns segundos e tente novamente.'
-                    :
-                    props.overview === undefined
-                    ? 'Clique no botão (Obter sugestão) para receber uma sugestão aleatória do que assistir.'
+                    ? ' Ops... parece que nosso serviço está indisponível no momento, agurade alguns segundos e tente novamente.'
+                    : props.overview === undefined
+                    ? ' Clique no botão (Obter sugestão) para receber uma sugestão aleatória do que assistir.'
                     : props.overview === ''
-                    ? 'Perdão ainda nâo temos a sinopse. Mas como pedido de desculpas queremos que você clique denovo e receba uma nova sugestão.'
+                    ? ' Perdão ainda nâo temos a sinopse. Mas como pedido de desculpas queremos que você clique denovo e receba uma nova sugestão.'
                     : props.overview 
                 }
             </h3>
